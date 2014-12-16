002116fdfddf1605348f3dc2246cc88bea7e36c6
Vincent Cogne <xpac27@gmail.com>
2014-12-16 23:40:25 +0100

    add a test

--- proto3.rb
+++ proto3.rb
@@ -27,47 +27,79 @@ class World
     @masks = [];
     @components_a = []
     @components_b = []
+    @components_a_map = []
+    @components_b_map = []
   end
 
   def add_entity mask
+    index = @masks.size
+
     @masks << mask
-    @components_a << ComponentA.new
-    @components_b << ComponentB.new
-    @masks.size - 1
+    @components_a_map << -1
+    @components_b_map << -1
+
+    add_entity_component index, mask
+
+    return index
   end
 
-  def add_entity_component entity, component
-    @components_a[entity] = ComponentA.new if component == COMPONENT_A
-    @components_b[entity] = ComponentB.new if component == COMPONENT_B
-    @masks[entity] = @masks[entity] | component
+  def has_entity_component entity, mask
+    return @masks[entity] & mask > 0
   end
 
-  def has_entity_component entity, component
-    @masks[entity] & component > 0
+  def get_entity_component entity, mask
+    return @components_a[@components_a_map[entity]] if mask == COMPONENT_A
+    return @components_b[@components_b_map[entity]] if mask == COMPONENT_B
   end
 
-  def get_entity_component entity, component
-    return @components_a[entity] if component == COMPONENT_A
-    return @components_b[entity] if component == COMPONENT_B
+  def add_entity_component entity, mask
+    if mask & COMPONENT_A > 0 
+      if @masks[entity] & mask == 0
+        @components_a[@components_a_map[entity]] == ComponentA.new 
+      else
+        @components_a << ComponentA.new 
+        @components_a_map[entity] = @components_a.size - 1
+      end
+    end
+
+    if mask & COMPONENT_B > 0 
+      if @masks[entity] & mask == 0
+        @components_b[@components_b_map[entity]] == ComponentB.new 
+      else
+        @components_b << ComponentB.new 
+        @components_b_map[entity] = @components_b.size - 1
+      end
+    end
+
+    @masks[entity] = @masks[entity] | mask
   end
 
-  def del_entity_component entity, component
-    @masks[entity] = @masks[entity] ^ component
-  end
+  # def del_entity_component entity, mask
+    # @masks[entity] = @masks[entity] ^ mask
+  # end
 
-  def del_entity entity
-    @masks[entity] = COMPONENT_X
-  end
+  # def del_entity entity
+    # @masks[entity] = COMPONENT_X
+  # end
 end
 
 w = World.new
 
-e1 = w.add_entity COMPONENT_A | COMPONENT_B
+e1 = w.add_entity COMPONENT_A
+e2 = w.add_entity COMPONENT_A | COMPONENT_B
+
+# w.get_entities COMPONENT_A
+# w.get_entities COMPONENT_A | COMPONENT_B
 
 w.get_entity_component e1, COMPONENT_A
-w.get_entity_component e1, COMPONENT_B
+w.get_entity_component e2, COMPONENT_A
+w.get_entity_component e2, COMPONENT_B
 
-w.del_entity_component e1, COMPONENT_A
-w.del_entity_component e1, COMPONENT_B
+w.add_entity_component e1, COMPONENT_A
 
-w.del_entity e1
+w.get_entity_component e1, COMPONENT_A
+
+# w.del_entity_component e1, COMPONENT_A
+# w.del_entity_component e1, COMPONENT_B
+
+# w.del_entity e1
