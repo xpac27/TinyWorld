c0406ead5e1951c5b177471bd6619ae27565e70c
Vincent Cogne <xpac27@gmail.com>
2015-03-22 15:43:55 +0100

    add tests for EntityManager

--- tests/src/test_EntityManager.cpp
+++ tests/src/test_EntityManager.cpp
@@ -1,13 +1,12 @@
 #include "catch.hpp"
 #include "ecs/EntityManager.hpp"
-#include "components/Life.hpp"
-#include "components/Position.hpp"
 
 namespace
 {
-    SCENARIO("EntitiesManager" "[addEntity]") {
+    SCENARIO("EntityManager" "[addEntity]")
+    {
         GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
+            ECS::EntityManager entityManager;
 
             WHEN("No entity are added") {
                 THEN("addEntity returns 0") {
@@ -15,44 +14,24 @@ namespace
                 }
             }
 
-            WHEN("3 entities are added") {
-                entityManager.addEntity();
-                entityManager.addEntity();
+            WHEN("1 entities are added") {
                 entityManager.addEntity();
 
-                THEN("addEntity returns 3") {
-                    CHECK(entityManager.addEntity() == 3);
+                THEN("addEntity returns 1") {
+                    CHECK(entityManager.addEntity() == 1);
                 }
             }
         }
     }
 
-    SCENARIO("EntitiesManager" "[resetEntity]") {
+    SCENARIO("EntityManager" "[getTotal]")
+    {
         GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
+            ECS::EntityManager entityManager;
 
             WHEN("No entity are added") {
-                THEN("Call to delEntity is invalid") {
-                    CHECK_THROWS(entityManager.resetEntity(0));
-                }
-            }
-
-            WHEN("1 entities is added") {
-                ECS::Index e = entityManager.addEntity();
-                THEN("Call to delEntity is valid") {
-                    CHECK_NOTHROW(entityManager.resetEntity(e));
-                }
-            }
-        }
-    }
-
-    SCENARIO("EntitiesManager" "[getEntityCount]") {
-        GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
-
-            WHEN("No entity are added") {
-                THEN("getEntityCount is 0") {
-                    CHECK(entityManager.getEntityCount() == 0);
+                THEN("getTotal returns 0") {
+                    CHECK(entityManager.getTotal() == 0);
                 }
             }
 
@@ -61,87 +40,8 @@ namespace
                 entityManager.addEntity();
                 entityManager.addEntity();
 
-                THEN("getEntityCount is 3") {
-                    CHECK(entityManager.getEntityCount() == 3);
-                }
-            }
-        }
-    }
-
-    SCENARIO("EntitiesManager" "[addComponent]") {
-        GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
-
-            THEN("Call to addComponent is invalid") {
-                CHECK_THROWS(entityManager.addComponent<Position>(1));
-            }
-
-            WHEN("1 entity added") {
-                ECS::Index e = entityManager.addEntity();
-
-                THEN("Adding a Life component is valid") {
-                    CHECK_NOTHROW(entityManager.addComponent<Life>(e));
-                }
-
-                WHEN("Life component added to entity") {
-                    entityManager.addComponent<Life>(e);
-
-                    THEN("Adding a Life component again is invalid") {
-                        CHECK_THROWS(entityManager.addComponent<Life>(e));
-                    }
-                    THEN("Adding a Position component is valid") {
-                        CHECK_NOTHROW(entityManager.addComponent<Position>(e));
-                    }
-                }
-            }
-        }
-    }
-
-    SCENARIO("EntitiesManager" "[getComponent]") {
-        GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
-
-            WHEN("1 entity added") {
-                ECS::Index e = entityManager.addEntity();
-
-                THEN("Get Life component is invalid") {
-                    CHECK_THROWS(entityManager.getComponent<Position>(e));
-                }
-
-                WHEN("1 Life component added to entity") {
-                    entityManager.addComponent<Life>(e);
-
-                    THEN("Get Life component is valid") {
-                        CHECK_NOTHROW(entityManager.getComponent<Life>(e));
-                    }
-                    THEN("Get Position component is invalid") {
-                        CHECK_THROWS(entityManager.getComponent<Position>(e));
-                    }
-                }
-            }
-        }
-    }
-
-    SCENARIO("EntitiesManager" "[delComponent]") {
-        GIVEN("An EntityManager") {
-            ECS::EntitiesManager entityManager;
-
-            WHEN("1 entity added") {
-                ECS::Index e = entityManager.addEntity();
-
-                THEN("Call to delComponent is invalid") {
-                    CHECK_THROWS(entityManager.delComponent<Position>(e));
-                }
-
-                WHEN("1 Life component added to entity") {
-                    entityManager.addComponent<Life>(e);
-
-                    THEN("Del Life component is valid") {
-                        CHECK_NOTHROW(entityManager.delComponent<Life>(e));
-                    }
-                    THEN("Del Position component is valid") {
-                        CHECK_THROWS(entityManager.delComponent<Position>(e));
-                    }
+                THEN("getTotal returns 3") {
+                    CHECK(entityManager.getTotal() == 3);
                 }
             }
         }
